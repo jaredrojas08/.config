c773701bbc024ef2d3c76a6d336d3270d9f04e64
jaredrojas08 <jaredrojas08@gmail.com>
2026-03-15 16:01:15 -0400

    feat: add lsp to nvim and code action ui

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require('mason').setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = { "lua_ls", "pyright" }
+      })
+    end
+  },
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      vim.lsp.enable('lua_ls', 'pyright')
+
+      vim.diagnostic.config({
+        virtual_text = true,
+        signs = true,
+        underline = true,
+      })
+
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
