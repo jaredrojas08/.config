ca5b6c8405266ff80baae1f8e21df121f17aad55
jaredrojas08 <jaredrojas08@gmail.com>
2026-03-15 14:15:05 -0400

    feat: add modularization to config and lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -20,40 +14,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  {
-  'embark-theme/vim',
-  lazy = false,
-  priority = 1000,
-  name = 'embark'
-  },
-  {
-    'nvim-telescope/telescope.nvim', version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-   }
-},
-{
-  'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
-}
-}
-local opts = {}
+require("vim-options")
+require("lazy").setup("plugins")
 
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin") 
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local config = require("nvim-treesitter") 
-config.setup({
-  ensure_installed = {"lua", "javascript"},
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
-vim.cmd.colorscheme "embark"
---
+++ nvim/lua/plugins/embark.lua
@@ -0,0 +1,9 @@
+return {
+  'embark-theme/vim',
+  lazy = false,
+  priority = 1000,
+  name = 'embark',
+  config = function()
+    vim.cmd.colorscheme "embark"
+  end
+}
