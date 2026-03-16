b57fb793ab56a1d8382d575916e32d2e486e1cda
jaredrojas08 <jaredrojas08@gmail.com>
2026-03-16 03:25:24 -0400

    feat: add ocaml LSP and debugger

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -5,6 +5,7 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.ocamlformat,
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.black,
