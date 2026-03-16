43c69b40b1b71da49afbe99b376e0d73aef81446
jaredrojas08 <jaredrojas08@gmail.com>
2026-03-16 02:37:15 -0400

    feat: add C# LSP and debugging

--- nvim/lua/plugins/debugging.lua
+++ nvim/lua/plugins/debugging.lua
@@ -12,6 +12,22 @@ return {
     require("dap-go").setup()
     require("dapui").setup()
 
+    dap.adapters.coreclr = {
+      type = "executable",
+      command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
+      args = { "--interpreter=vscode" },
+    }
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "launch - netcoredbg",
+        request = "launch",
+        program = function()
+          return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
+        end,
+      },
+    }
+
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
 		end
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,40 +1,40 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    config = function()
-      require('mason').setup()
-    end
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "pyright" }
-      })
-    end
-  },
-  {
-    'neovim/nvim-lspconfig',
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      vim.lsp.config('*', { capabilities = capabilities })
-      vim.lsp.enable('lua_ls', 'pyright')
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "omnisharp" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.config("*", { capabilities = capabilities })
+			vim.lsp.enable("lua_ls", "pyright", "omnisharp")
 
-      vim.diagnostic.config({
-        virtual_text = true,
-        signs = true,
-        underline = true,
-      })
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+			})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
