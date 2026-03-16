c2ed0eaaf7aa88bf7508fa87f76b769c4bdde9d8
jaredrojas08 <jaredrojas08@gmail.com>
2026-03-16 16:01:33 -0400

    style: add metals to lsp-config since metals is lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -23,7 +23,7 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			vim.lsp.config("*", { capabilities = capabilities })
-			vim.lsp.enable("lua_ls", "pyright", "omnisharp", "clangd", "ocamllsp", "asm-lsp" )
+			vim.lsp.enable("lua_ls", "pyright", "omnisharp", "clangd", "ocamllsp", "asm-lsp")
 
 			vim.diagnostic.config({
 				virtual_text = true,
@@ -36,5 +36,32 @@ return {
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 		end,
+	},
+
+	{
+		{
+			"scalameta/nvim-metals",
+			dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/cmp-nvim-lsp" },
+			ft = { "scala", "sbt", "java" },
+			opts = function()
+				local metals_config = require("metals").bare_config()
+				metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+				metals_config.settings = {
+					showImplicitArguments = true,
+					excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+				}
+				return metals_config
+			end,
+			config = function(self, metals_config)
+				local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = self.ft,
+					callback = function()
+						require("metals").initialize_or_attach(metals_config)
+					end,
+					group = nvim_metals_group,
+				})
+			end,
+		},
 	},
 }
