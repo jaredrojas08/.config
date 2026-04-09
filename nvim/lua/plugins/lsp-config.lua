388b6296e078902f8fe8eecba154c6c6b0f6705c
jaredrojas08 <jaredrojas08@gmail.com>
2026-04-09 18:45:56 -0400

    feat: add svelte lsp

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,95 +1,97 @@
 return {
-    {
-        "mason-org/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {},
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "pyright",
-                    "omnisharp",
-                    "clangd",
-                    "ocamllsp",
-                    "asm_lsp",
-                    "jdtls",
-                    "ts_ls",
-                    "html",
-                    "cssls",
-                    "marksman",
-                    "taplo",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            vim.lsp.config("*", { capabilities = capabilities })
-            vim.lsp.config("omnisharp", {})
-            vim.lsp.enable(
-                "lua_ls",
-                "pyright",
-                "omnisharp",
-                "clangd",
-                "ocamllsp",
-                "asm_lsp",
-                "jdtls",
-                "ts_ls",
-                "sourcekit",
-                "html",
-                "cssls",
-                "marksman",
-                "taplo"
-            )
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"omnisharp",
+					"clangd",
+					"ocamllsp",
+					"asm_lsp",
+					"jdtls",
+					"ts_ls",
+					"html",
+					"cssls",
+					"marksman",
+					"taplo",
+					"svelte",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			vim.lsp.config("*", { capabilities = capabilities })
+			vim.lsp.config("omnisharp", {})
+			vim.lsp.enable(
+				"lua_ls",
+				"pyright",
+				"omnisharp",
+				"clangd",
+				"ocamllsp",
+				"asm_lsp",
+				"jdtls",
+				"ts_ls",
+				"sourcekit",
+				"html",
+				"cssls",
+				"marksman",
+				"taplo",
+				"svelte"
+			)
 
-            vim.diagnostic.config({
-                virtual_text = true,
-                signs = true,
-                underline = true,
-            })
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 
-    {
-        {
-            "scalameta/nvim-metals",
-            dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/cmp-nvim-lsp" },
-            ft = { "scala", "sbt" },
-            opts = function()
-                local metals_config = require("metals").bare_config()
-                metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-                metals_config.settings = {
-                    showImplicitArguments = true,
-                    excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
-                }
-                return metals_config
-            end,
-            config = function(self, metals_config)
-                local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-                vim.api.nvim_create_autocmd("FileType", {
-                    pattern = { "scala", "sbt" },
-                    callback = function()
-                        require("metals").initialize_or_attach(metals_config)
-                    end,
-                    group = nvim_metals_group,
-                })
-            end,
-        },
-    },
+	{
+		{
+			"scalameta/nvim-metals",
+			dependencies = { "nvim-lua/plenary.nvim", "hrsh7th/cmp-nvim-lsp" },
+			ft = { "scala", "sbt" },
+			opts = function()
+				local metals_config = require("metals").bare_config()
+				metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+				metals_config.settings = {
+					showImplicitArguments = true,
+					excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+				}
+				return metals_config
+			end,
+			config = function(self, metals_config)
+				local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+				vim.api.nvim_create_autocmd("FileType", {
+					pattern = { "scala", "sbt" },
+					callback = function()
+						require("metals").initialize_or_attach(metals_config)
+					end,
+					group = nvim_metals_group,
+				})
+			end,
+		},
+	},
 }
